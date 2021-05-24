cc8dc16e9db40637361e6548edc90542c9a94d35
kl <datesaver1234@gmail.com>
2021-05-25 03:27:29 +0900

    add show image

--- main.py
+++ main.py
@@ -6,4 +6,22 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+
+def show_image():
+    plt.figure()
+    plt.imshow(train_images[1])
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), \
+    (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
+               'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print(train_images.shape, train_labels.shape)
+show_image()
