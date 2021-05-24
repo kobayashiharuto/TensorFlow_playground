1f9fe095e993014f5b14824c77007a7d99465f7e
kl <datesaver1234@gmail.com>
2021-05-25 04:42:01 +0900

    fitting

--- main.py
+++ main.py
@@ -15,6 +15,19 @@ def show_image():
     plt.show()
 
 
+def show_images():
+    plt.figure(figsize=(10, 10))
+
+    for i in range(25):
+        plt.subplot(5, 5, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i]])
+    plt.show()
+
+
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), \
@@ -24,4 +37,22 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress',
                'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 print(train_images.shape, train_labels.shape)
-show_image()
+
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+print(predictions[0], np.argmax(predictions[0]))
